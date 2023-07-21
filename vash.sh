d9856474c1b883365262080c5c2fa364357db013
byaslam <126193860+byaslam@users.noreply.github.com>
2023-07-22 00:33:33 +0800

    Add files via upload

---
+++ vash.sh
@@ -0,0 +1,41 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay by aslam
+echo $green"Selamat Datang Di VIP By Aslam"
+echo
+sleep 2
+echo $yellow"Silahkan pilih auto ping ff nya"
+echo
+echo $cyan"1.) untuk freefire biasa dan biasa"
+echo
+echo $blue"0.) keluar ajah"
+echo
+echo $white
+read -p "pilih mana : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay aslam
+echo $green"Stabil headshot dan jaringan by aslam"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $yellow
+figlet "dada anjing"
+echo $white"pake lagi lain waktu yah asu"
+sleep 3
+exit
+fi
